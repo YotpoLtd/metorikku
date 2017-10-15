7977fd37c9c70289c4939dbb9ebc9002696f1a7f
Ron Barabash <rbarabash@yotpo.com>
2017-10-15 15:40:39 +0300

    refactor(TODOs): added todos

--- src/main/scala/com/yotpo/metorikku/metrics/calculation/Calculation.scala
+++ src/main/scala/com/yotpo/metorikku/metrics/calculation/Calculation.scala
@@ -13,23 +13,33 @@ import com.yotpo.metorikku.metrics.udf.UDFUtils
   */
 
 class Calculation(calculationDir: File, metricSparkSession: MetricSparkSession) {
+  //TODO should be injectable
   val calculationConfig = metricSparkSession.calculationConfig
+  //TODO Parse Metrics?
   val metrics = initMetrics(calculationDir)
 
   def initMetrics(calculationDir: File): Seq[Metric] = {
     val allMetrics = MqlFileUtils.getMetrics(calculationDir)
+    //TODO remove all the intersection stuff
     val metricsToCalculate = FileUtils.intersect(allMetrics, calculationConfig.metricsToCalculate)
+    //TODO move to function
     val udfs = UDFUtils.getAllUDFsInPath(calculationDir.getPath + "/udfs/")
     udfs.foreach(udf => metricSparkSession.registerUdf(udf))
     metricsToCalculate.map(metricFile => {
       val metricConfig = FileUtils.jsonFileToObject[MetricConfig](metricFile.getAbsolutePath)
+      //TODO remove calculation config and make it injectable yay! also remove metricFile.getParent
       new Metric(metricConfig, calculationConfig, metricFile.getParent)
     })
+    //TODO EXPLICIT RETURN METRICS
   }
 
   def run(): Unit = {
     metrics.foreach(metric => {
+      //TODO No need to pass the session use get
+      //TODO RENAME TO SqlStepCalculator or something
+      //TODO WE SHOULD DECIDE THE TYPE OF CALCULATION HERE, MORE LIKE A STRATAGY PATTERN
       val df = new ConfigurableCalculator(metric).calculate(metricSparkSession.getSparkSession().sqlContext, calculationConfig.previewStepLines)
+      //TODO REFACTOR ALL THE PREVIEW STUFF MECHANISM
       if (calculationConfig.previewLines > 0) {
         df.show(calculationConfig.previewLines, truncate = false)
       }
