5402de885f36fd5b749b52611610763e2d9ee88b
Ron Barabash <rbarabash@yotpo.com>
2017-10-15 15:40:39 +0300

    refactor(TODOs): added todos

--- src/main/scala/com/yotpo/metorikku/metrics/calculation/Metric.scala
+++ src/main/scala/com/yotpo/metorikku/metrics/calculation/Metric.scala
@@ -8,9 +8,11 @@ import com.yotpo.metorikku.metrics.output.MetricOutputHandler
   */
 case class MetricConfig(steps: List[Map[String, String]], output: List[Map[String, Any]])
 
+//TODO USE OPTIONS INSTEAD OF NULLS
 class Metric(metricConfig: MetricConfig, calculationConfig: GlobalCalculationConfig = null, metricDirAbsolutePath: String = "") {
   val date =  if(calculationConfig==null) "" else calculationConfig.runningDate
   val steps = metricConfig.steps.map(new Sql(_, metricDirAbsolutePath))
+  //TODO remove calculation config from all the places
   val outputs = if(calculationConfig==null) null else metricConfig.output.map(new MetricOutputHandler(_, calculationConfig))
 }
 
