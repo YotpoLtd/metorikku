c40d255aa89a53a99b5a83f157d6c6331f11d5ad
ofirventura <ofirventura@gmail.com>
2017-10-23 12:13:12 +0300

    feat(Metorikku): json output writer (#4)
    * feat(Metorikku): json output writer
    
    * fix(Metorikku): fix code review comment regarding coalesce
    
    * remove redundant file

--- src/main/scala/com/yotpo/metorikku/output/writers/csv/CSVOutputWriter.scala
+++ src/main/scala/com/yotpo/metorikku/output/writers/csv/CSVOutputWriter.scala
@@ -7,16 +7,16 @@ import scala.collection.mutable
 
 class CSVOutputWriter(metricOutputOptions: mutable.Map[String, String], outputFilePath: String) extends MetricOutputWriter {
 
-  case class CSVOutputProperties(saveMode: SaveMode, path: String, repartition: Boolean, csvOptions: Map[String, String])
+  case class CSVOutputProperties(saveMode: SaveMode, path: String, coalesce: Boolean, csvOptions: Map[String, String])
 
   val baseOutputPath = outputFilePath
   val props = metricOutputOptions("outputOptions").asInstanceOf[Map[String, String]]
-  val repartition = props.getOrElse("repartition", true).asInstanceOf[Boolean]
+  val coalesce = props.getOrElse("coalesce", true).asInstanceOf[Boolean]
   val csvOptions = props.getOrElse("csvOptions", Map("" -> "")).asInstanceOf[Map[String, String]]
-  val csvOutputOptions = CSVOutputProperties(SaveMode.valueOf(props("saveMode")), props("path"), repartition, csvOptions)
+  val csvOutputOptions = CSVOutputProperties(SaveMode.valueOf(props("saveMode")), props("path"), coalesce, csvOptions)
 
   override def write(dataFrame: DataFrame): Unit = {
-    val df = if (csvOutputOptions.repartition) dataFrame.repartition(1) else dataFrame
+    val df = if (csvOutputOptions.coalesce) dataFrame.coalesce(1) else dataFrame
     df.write.mode(csvOutputOptions.saveMode).options(csvOptions).csv(baseOutputPath + "/" + csvOutputOptions.path)
   }
 }
