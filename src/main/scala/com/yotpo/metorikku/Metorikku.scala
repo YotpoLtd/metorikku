3ccf457f63469d666bd502e55f94799cd8050a6a
lyogev <lyogev@yotpo.com>
2017-10-24 12:19:03 +0300

    feat(logs): added first logs to metorikku

--- src/main/scala/com/yotpo/metorikku/Metorikku.scala
+++ src/main/scala/com/yotpo/metorikku/Metorikku.scala
@@ -3,17 +3,24 @@ package com.yotpo.metorikku
 import com.yotpo.metorikku.configuration.{YAMLConfiguration, YAMLConfigurationParser}
 import com.yotpo.metorikku.metric.MetricSet
 import com.yotpo.metorikku.session.Session
+import org.apache.log4j.LogManager
+import scopt.OptionParser
 
 /**
   * Metorikku - runs Spark SQL queries on various data sources and exports the results
   */
 object Metorikku extends App {
+  val log = LogManager.getLogger(this.getClass)
+
+  log.info("Starting Metorikku - Parsing configuration")
+
   val config: YAMLConfiguration = YAMLConfigurationParser.parse(args)
   Session.init(config)
   start
 
   def start() {
     Session.getConfiguration.metrics.foreach(metric => {
+      log.info(s"Starting to calculate for metric ${metric}")
       val metricSet = new MetricSet(metric)
       metricSet.run()
       metricSet.write()
--- src/main/scala/com/yotpo/metorikku/calculators/SqlStepCalculator.scala
+++ src/main/scala/com/yotpo/metorikku/calculators/SqlStepCalculator.scala
@@ -2,14 +2,17 @@ package com.yotpo.metorikku.calculators
 
 import com.yotpo.metorikku.metric.Metric
 import com.yotpo.metorikku.session.Session
+import org.apache.log4j.LogManager
 import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.functions.lit
 
 class SqlStepCalculator(metric: Metric) extends Calculator {
+  val log = LogManager.getLogger(this.getClass)
+
   override def calculate(): DataFrame = {
     val sqlContext = Session.getSparkSession.sqlContext
     var stepResult = sqlContext.emptyDataFrame
     for (step <- metric.steps) {
+      log.debug(s"Calculating step ${step.dataFrameName} from metric ${metric.metricDir}")
       stepResult = step.actOnDataFrame(sqlContext)
       printStep(stepResult, step.dataFrameName)
     }
@@ -18,9 +21,9 @@ class SqlStepCalculator(metric: Metric) extends Calculator {
 
   private def printStep(stepResult: DataFrame, stepName: String): Unit = {
     if (Session.getConfiguration.showPreviewLines > 0) {
-      //TODO: write to a proper logger instead of print
-      println(s"Previewing step: $stepName")
+      log.info(s"Previewing step: $stepName")
       stepResult.printSchema()
+      stepResult.printSchema
       stepResult.show(Session.getConfiguration.showPreviewLines, truncate = false)
     }
   }
