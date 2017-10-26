5f17a2922aa72b49189c4c88f883978402adc6dd
Avichay Etzioni <avichay@yotpo.com>
2017-10-26 18:32:54 +0300

    feat(Writer validations):Added metric validations

--- src/main/scala/com/yotpo/metorikku/output/writers/segment/SegmentOutputWriter.scala
+++ src/main/scala/com/yotpo/metorikku/output/writers/segment/SegmentOutputWriter.scala
@@ -1,51 +1,57 @@
 package com.yotpo.metorikku.output.writers.segment
 
+import com.google.gson.Gson
 import com.segment.analytics.Analytics
-import com.segment.analytics.messages.IdentifyMessage
+import com.segment.analytics.messages.{IdentifyMessage, TrackMessage}
 import com.yotpo.metorikku.configuration.outputs.Segment
-import com.yotpo.metorikku.instrumentation.Instrumentation
-import org.apache.log4j.LogManager
 import com.yotpo.metorikku.instrumentation.Instrumentation
 import com.yotpo.metorikku.output.MetricOutputWriter
 import org.apache.spark.sql.DataFrame
 
-import scala.collection.JavaConversions._
 import scala.collection.mutable
 
 class SegmentOutputWriter(metricOutputOptions: mutable.Map[String, String], segmentOutputConf: Option[Segment]) extends MetricOutputWriter {
 
-  case class SegmentOutputProperties(keyColumn: String)
+  case class SegmentOutputProperties(eventType: String, keyColumn: String, eventName: String)
 
   val props = metricOutputOptions("outputOptions").asInstanceOf[Map[String, String]]
-  val segmentOutputOptions = SegmentOutputProperties(props("keyColumn"))
+  val eventType = props.getOrElse("eventType", "identify")
+  val eventName = props.getOrElse("eventName", "")
+  val keyColumn = props.getOrElse("keyColumn", "")
+
+  if (eventType == "identify") setMandatoryArguments("keyColumn") else setMandatoryArguments("keyColumn", "eventName")
+
+  val segmentOutputOptions = SegmentOutputProperties(eventType, keyColumn, eventName)
 
   override def write(dataFrame: DataFrame): Unit = {
     segmentOutputConf match {
       case Some(segmentOutputConf) =>
         val segmentApiKey = segmentOutputConf.apiKey
         val columns = dataFrame.columns.filter(_ != segmentOutputOptions.keyColumn)
-
-        dataFrame.foreachPartition(partition => {
+        dataFrame.toJSON.foreachPartition(partition => {
           val blockingFlush = BlockingFlush.create
           val analytics: Analytics = Analytics.builder(segmentApiKey).plugin(blockingFlush.plugin).build()
           partition.foreach(row => {
-            val userId = row.getAs[Any](segmentOutputOptions.keyColumn)
+            val eventTraits = new Gson().fromJson(row, classOf[java.util.Map[String, Object]])
+            val userId = eventTraits.get(segmentOutputOptions.keyColumn).asInstanceOf[Double].toInt
+            eventTraits.remove(segmentOutputOptions.keyColumn)
             try {
-              val eventTraits = new mutable.HashMap[String, String]()
-              row.getValuesMap[Any](columns).foreach { case (key, value) => eventTraits.put(key, value.toString) }
-              analytics.enqueue(IdentifyMessage.builder()
-                .userId(userId.toString)
-                .traits(eventTraits)
-              )
-              val successEvent = {
-                Map("user_id" -> userId.toString, "type" -> "success", "metric" -> metricOutputOptions("dataFrameName"))
+
+              segmentOutputOptions.eventType match {
+                case "track" =>
+                  analytics.enqueue(TrackMessage.builder(segmentOutputOptions.eventName)
+                    .userId(userId.toString)
+                    .properties(eventTraits)
+                  )
+                case "identify" =>
+                  analytics.enqueue(IdentifyMessage.builder()
+                    .userId(userId.toString)
+                    .traits(eventTraits)
+                  )
               }
               Instrumentation.segmentWriterSuccess.inc(1)
             } catch {
               case exception: Throwable =>
-                val failedEvent = {
-                  Map("user_id" -> userId.toString, "type" -> "error", "metric" -> metricOutputOptions("dataFrameName"))
-                }
                 Instrumentation.segmentWriterFailure.inc(1)
             }
           })
