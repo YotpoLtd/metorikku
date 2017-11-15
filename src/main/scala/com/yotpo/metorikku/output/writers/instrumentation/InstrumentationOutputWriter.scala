544d47851a3be77015e647d72d5bafb6cd7ea14c
Ofir Ventura <oventura@yotpo.com>
2017-11-15 18:58:08 +0200

    fix(instrumentation): fix instrumentation writer - numeric types

--- src/main/scala/com/yotpo/metorikku/output/writers/instrumentation/InstrumentationOutputWriter.scala
+++ src/main/scala/com/yotpo/metorikku/output/writers/instrumentation/InstrumentationOutputWriter.scala
@@ -8,40 +8,40 @@ import org.apache.spark.sql.DataFrame
 import scala.collection.mutable
 
 
-object InstrumentationOutputWriter {
-  val log = LogManager.getLogger(this.getClass)
-}
-
 class InstrumentationOutputWriter(metricOutputOptions: mutable.Map[String, String], metricName: String) extends MetricOutputWriter {
   case class InstrumentationOutputProperties(keyColumn: String)
   val props = metricOutputOptions("outputOptions").asInstanceOf[Map[String, String]]
   val dataFrameName = metricOutputOptions("dataFrameName")
   val keyColumnProperty = InstrumentationOutputProperties(props("keyColumn")).keyColumn
+  @transient val log = LogManager.getLogger(this.getClass)
 
   override def write(dataFrame: DataFrame): Unit = {
     val counterNames = Array(metricName, dataFrameName)
-    val columns = dataFrame.schema.fields.filter(_.name != keyColumnProperty).zipWithIndex
+    val columns = dataFrame.schema.fields.zipWithIndex
     val indexOfKeyCol = dataFrame.schema.fieldNames.indexOf(keyColumnProperty)
+    log.info(s"Starting to write Instrumentation on data frame: ${dataFrameName}")
     dataFrame.foreach(row => {
       for((column,i) <- columns) {
         try{
           val valueOfRowAtCurrentCol = row.get(i)
-          if (valueOfRowAtCurrentCol != null){
-            if (!keyColumnProperty.isEmpty){
+          if (valueOfRowAtCurrentCol != null && column.name != keyColumnProperty){
+            val isNumeric = classOf[Number].isAssignableFrom(valueOfRowAtCurrentCol.getClass())
+            if (!keyColumnProperty.isEmpty && isNumeric){
               val valueOfRowAtKeyCol = row.get(indexOfKeyCol)
               if (valueOfRowAtKeyCol != null){
                 val counterTitles = counterNames :+ column.name :+ keyColumnProperty :+ valueOfRowAtKeyCol.asInstanceOf[AnyVal].toString
                 lazy val fieldCounter: SparkGauge = InstrumentationUtils.createNewGauge(counterTitles)
-                fieldCounter.set(valueOfRowAtCurrentCol.asInstanceOf[AnyVal])
+                fieldCounter.set(valueOfRowAtCurrentCol.asInstanceOf[Number].doubleValue())
+
               }
-            } else {
+            } else if (valueOfRowAtCurrentCol != null && isNumeric){
               lazy val columnCounter: SparkGauge = InstrumentationUtils.createNewGauge(counterNames :+ column.name)
-              columnCounter.set(valueOfRowAtCurrentCol.asInstanceOf[AnyVal])
+              columnCounter.set(valueOfRowAtCurrentCol.asInstanceOf[Number].doubleValue())
             }
           }
         } catch {
           case ex: Throwable =>{
-            InstrumentationOutputWriter.log.error(s"failed to write instrumentation on data frame: ${dataFrameName} for row: ${row.toString()} on column: ${column.name}", ex)
+            log.error(s"failed to write instrumentation on data frame: ${dataFrameName} for row: ${row.toString()} on column: ${column.name}", ex)
           }
         }
       }
