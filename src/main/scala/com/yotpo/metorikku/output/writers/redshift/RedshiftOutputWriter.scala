21ce01281c4cbeb1bed04b3302adf26b35413f01
doronporat <dporat@yotpo.com>
2017-12-01 22:20:41 +0200

    refactor(RedshiftOutputWriter): use dbOptions instead of props when possible

--- src/main/scala/com/yotpo/metorikku/output/writers/redshift/RedshiftOutputWriter.scala
+++ src/main/scala/com/yotpo/metorikku/output/writers/redshift/RedshiftOutputWriter.scala
@@ -11,26 +11,25 @@ import scala.collection.mutable
 
 class RedshiftOutputWriter(metricOutputOptions: mutable.Map[String, String], redshiftDBConf: Option[Redshift]) extends MetricOutputWriter {
 
-  case class RedshiftOutputProperties(saveMode: SaveMode, dbTable: String, maxStringSize: String)
+  case class RedshiftOutputProperties(saveMode: SaveMode, dbTable: String, maxStringSize: String, extraCopyOptions: String, postActions: String)
 
   val log = LogManager.getLogger(this.getClass)
   val props = metricOutputOptions("outputOptions").asInstanceOf[Map[String, String]]
-  val dbOptions = RedshiftOutputProperties(SaveMode.valueOf(props("saveMode")), props("dbTable"), props("maxStringSize"))
+  val dbOptions = RedshiftOutputProperties(SaveMode.valueOf(props("saveMode")), props("dbTable"), props("maxStringSize"), props("extraCopyOptions"), props("postActions"))
 
   override def write(dataFrame: DataFrame): Unit = {
     redshiftDBConf match {
       case Some(redshiftDBConf) =>
         import dataFrame.sparkSession.implicits._
 
-        // Calculate actual Schema For Varchar columns
         var df = dataFrame
 
         df.schema.fields.filter(f => f.dataType.isInstanceOf[StringType]).foreach(f => {
-          val maxlength = props match {
-            case _ if props.contains("maxStringSize") => props("maxStringSize").toInt
+          val maxlength = dbOptions match {
+            case _ if !dbOptions.maxStringSize.isEmpty => dbOptions.maxStringSize.toInt
             case _ =>  df.agg(max(length(df(f.name)))).as[Int].first
           }
-          df = df.withColumn(f.name, df(f.name).as(f.name, new MetadataBuilder().putLong("maxlength", maxlength).build()))
+          df = df.withColumn(f.name, df(f.name).as(f.name, new MetadataBuilder().putLong(dbOptions.maxStringSize, maxlength).build()))
         })
 
         log.info(s"Writing dataframe to Redshift' table ${props("dbTable")}")
@@ -41,11 +40,11 @@ class RedshiftOutputWriter(metricOutputOptions: mutable.Map[String, String], red
           .option("dbtable", dbOptions.dbTable)
           .mode(dbOptions.saveMode)
 
-        if (props.contains("postActions")) {
-          writer.option("postActions", props("postActions"))
+        if (!dbOptions.postActions.isEmpty) {
+          writer.option("postActions", dbOptions.postActions)
         }
-        if (props.contains("extracopyoptions")) {
-          writer.option("extracopyoptions", props("extracopyoptions"))
+        if (!dbOptions.extraCopyOptions.isEmpty) {
+          writer.option("extracopyoptions", dbOptions.extraCopyOptions)
         }
         writer.save()
 
