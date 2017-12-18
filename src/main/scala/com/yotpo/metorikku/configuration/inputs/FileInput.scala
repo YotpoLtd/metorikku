e215d5e7e582801655e500057c57ffed5e972e85
eyalkenig <eyalkenig@gmail.com>
2017-12-18 20:32:22 +0200

    refactor(Session):get redear from input

--- src/main/scala/com/yotpo/metorikku/configuration/inputs/FileInput.scala
+++ src/main/scala/com/yotpo/metorikku/configuration/inputs/FileInput.scala
@@ -1,5 +1,7 @@
 package com.yotpo.metorikku.configuration.inputs
+import com.yotpo.metorikku.input.InputTableReader
 
 case class FileInput(name: String, path: String) extends Input {
   override def getSequence: Seq[String] = Seq(path)
+  override def getReader(seq: Seq[String]): InputTableReader = InputTableReader(seq)
 }
