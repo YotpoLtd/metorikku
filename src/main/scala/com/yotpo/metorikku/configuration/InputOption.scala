4df5ed140ec32809649555f0d2f1b22dae546188
eyalkenig <eyalkenig@gmail.com>
2017-12-19 11:08:18 +0200

    feat(JdbcInput):added jdbc input

--- src/main/scala/com/yotpo/metorikku/configuration/InputOption.scala
+++ src/main/scala/com/yotpo/metorikku/configuration/InputOption.scala
@@ -3,6 +3,8 @@ package com.yotpo.metorikku.configuration
 import com.fasterxml.jackson.annotation.JsonProperty
 
 case class InputOption(@JsonProperty("type") inputType: String,
-                @JsonProperty("path") path: String,
-                @JsonProperty("template") template: String,
-                @JsonProperty("dateRange") dateRange: DateRange) {}
+                       @JsonProperty("path") path: String,
+                       @JsonProperty("template") template: String,
+                       @JsonProperty("dateRange") dateRange: DateRange,
+                       @JsonProperty("connection") connection: Connection,
+                       @JsonProperty("db_table") dbTable: String) {}
--- src/main/scala/com/yotpo/metorikku/configuration/inputs/InputFactory.scala
+++ src/main/scala/com/yotpo/metorikku/configuration/inputs/InputFactory.scala
@@ -7,10 +7,11 @@ object InputFactory {
     return inputOption.inputType match {
       case "file" => FileInput(name, inputOption.path)
       case "file_date_range" => FileDateRangeInput(name, inputOption.template, inputOption.dateRange)
+      case "jdbc" => JdbcInput(name, inputOption.connection, inputOption.dbTable)
       case _ => throw new UnknownInputTypeArgumentException(inputOption.inputType)
     }
   }
+  case class UnknownInputTypeArgumentException(private val message: String = "",
+                                               private val cause: Throwable = None.orNull)
+    extends Exception(message, cause)
 }
-case class UnknownInputTypeArgumentException(private val message: String = "",
-                                          private val cause: Throwable = None.orNull)
-  extends Exception(message, cause)
