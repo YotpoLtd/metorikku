eb644453b4decaa3e215c96a70b7800c0420b509
Shir <sbromberg@yotpo.com>
2018-09-04 21:20:43 +0300

    feat(parquet-writer): add stream writer

--- src/main/scala/com/yotpo/metorikku/output/writers/parquet/ParquetOutputWriter.scala
+++ src/main/scala/com/yotpo/metorikku/output/writers/parquet/ParquetOutputWriter.scala
@@ -3,19 +3,21 @@ package com.yotpo.metorikku.output.writers.parquet
 import com.yotpo.metorikku.configuration.outputs.File
 import com.yotpo.metorikku.output.MetricOutputWriter
 import org.apache.log4j.LogManager
+import org.apache.spark.sql.streaming.{OutputMode, Trigger}
 import org.apache.spark.sql.{DataFrame, SaveMode}
 
 
 class ParquetOutputWriter(props: Map[String, String], outputFile: Option[File]) extends MetricOutputWriter {
   val NO_REPARTITION = 0
-  case class ParquetOutputProperties(saveMode: SaveMode, path: String, partitionBy: Seq[String])
+  case class ParquetOutputProperties(saveMode: SaveMode, path: String, partitionBy: Seq[String], ProcessingTime: Long)
 
   val log = LogManager.getLogger(this.getClass)
   val partitionBy = props.getOrElse("partitionBy", Seq.empty).asInstanceOf[Seq[String]]
   val repartitionValue = props.getOrElse("repartition",NO_REPARTITION).asInstanceOf[Integer]
+  val processingTime = props.getOrElse("processingTime", 0L).asInstanceOf[Long]
   val parquetOutputOptions = ParquetOutputProperties(SaveMode.valueOf(props("saveMode")),
                                                      props("path"),
-                                                     partitionBy)
+                                                     partitionBy, processingTime)
 
   override def write(dataFrame: DataFrame): Unit = {
     outputFile match {
@@ -27,27 +29,29 @@ class ParquetOutputWriter(props: Map[String, String], outputFile: Option[File]) 
         if (parquetOutputOptions.partitionBy.nonEmpty) {
           writer = writer.partitionBy(parquetOutputOptions.partitionBy: _*)
         }
-        if (!dataFrame.isStreaming) {
-          log.info(s"Writing Parquet Dataframe to ${outputPath}")
-          var writer = dataFrame.write
-          if (parquetOutputOptions.partitionBy.nonEmpty) {
-            writer = writer.partitionBy(parquetOutputOptions.partitionBy: _*)
-          }
-          writer.mode(parquetOutputOptions.saveMode).parquet(outputPath)
-        }
-        else {
-          val writeStream = dataFrame.writeStream
-            .format("console")
-            .option("checkpointLocation", "/tmp/checkpoint")
-            .option("path", outputPath)
-            .outputMode("complete")
-            .start()
 
-          writeStream.awaitTermination()
-        }
+        writer.mode(parquetOutputOptions.saveMode).parquet(outputPath)
       case None => log.error(s"Parquet file configuration were not provided")
     }
 
   }
-}
 
+  override def writeStream(dataFrame: DataFrame): Unit = {
+    outputFile match {
+      case Some(fileConfig) =>
+        val outputPath = fileConfig.dir + "/" + parquetOutputOptions.path
+        log.info(s"Writing Dataframe to parquet $outputPath")
+        val stream = dataFrame.writeStream
+          .format("parquet")
+          .trigger(Trigger.ProcessingTime(parquetOutputOptions.ProcessingTime))
+          .option("path", outputPath)
+          .option("checkpointLocation", fileConfig.checkpointLocation.get)
+          .outputMode(parquetOutputOptions.saveMode.toString)
+
+        val query = stream.start()
+        query.awaitTermination()
+
+      case None =>
+    }
+  }
+}
