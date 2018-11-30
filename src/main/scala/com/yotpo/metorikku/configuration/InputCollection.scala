048a5541419ebd1ecbf07a2929412446f1a60ce3
lyogev <lyogev@yotpo.com>
2018-11-30 20:45:10 +0200

    feat(cassandra): add support for cassandra input

--- src/main/scala/com/yotpo/metorikku/configuration/InputCollection.scala
+++ src/main/scala/com/yotpo/metorikku/configuration/InputCollection.scala
@@ -1,18 +1,23 @@
 package com.yotpo.metorikku.configuration
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.yotpo.metorikku.configuration.input.{File, FileDateRange, JDBC, Kafka}
+import com.yotpo.metorikku.configuration.input._
 import com.yotpo.metorikku.input.Reader
 
 case class InputCollection(@JsonProperty("file") fileInput: Option[File],
                            @JsonProperty("file_date_range") fileDateRangeInput: Option[FileDateRange],
                            @JsonProperty("jdbc") jdbcInput: Option[JDBC],
-                           @JsonProperty("kafka") kafkaInput: Option[Kafka]) {
-  def getInput: Input = fileInput.getOrElse(fileDateRangeInput.getOrElse(jdbcInput.getOrElse(kafkaInput.getOrElse(Empty()))))
+                           @JsonProperty("kafka") kafkaInput: Option[Kafka],
+                           @JsonProperty("cassandra") cassandraInput: Option[Cassandra]) {
+  def getInput: Input = fileInput.getOrElse(
+    fileDateRangeInput.getOrElse(
+      jdbcInput.getOrElse(
+        kafkaInput.getOrElse(
+          cassandraInput.getOrElse(Empty())))))
 }
 
 object InputCollection {
-  def apply(): InputCollection = new InputCollection(None, None, None, None)
+  def apply(): InputCollection = new InputCollection(None, None, None, None, None)
 }
 
 case class UnknownInputTypeException(private val message: String = "",
