9edb3ee5c0caae6160b45492b7278bc3bf5edb3b
lyogev <lyogev@yotpo.com>
2018-12-02 16:12:29 +0200

    fix(docker): add default values and support for spark_worker_opts

--- docker/spark/scripts/entrypoint-master.sh
+++ docker/spark/scripts/entrypoint-master.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Default values
+SPARK_MASTER_PORT=${SPARK_MASTER_PORT:=7077}
+SPARK_WEBUI_PORT=${SPARK_WEBUI_PORT:=8080}
+SPARK_MASTER_HOST=${SPARK_MASTER_HOST:=spark-master}
+
 # Logs
 /scripts/init-logs-metrics.sh
 
