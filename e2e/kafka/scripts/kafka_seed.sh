8001526f65731f7e53a92e31c866ace760d25ee1
lyogev <lyogev@yotpo.com>
2018-12-22 18:43:17 +0200

    feat(e2e): update travis script to include e2e

--- e2e/kafka/scripts/kafka_seed.sh
+++ e2e/kafka/scripts/kafka_seed.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+KAFKA_BROKER=${KAFKA_BROKER:=kafka:9092}
+
 /scripts/wait_for_topic.sh
 
-kafka-console-producer.sh --request-required-acks 1 --broker-list kafka:9092 --topic ${TOPIC} < ${SEED_FILE}
-echo "Completed producing see to kafka topic ${TOPIC}"
+kafka-console-producer.sh --request-required-acks 1 --broker-list ${KAFKA_BROKER} --topic ${TOPIC} < ${SEED_FILE}
+echo "Completed producing seed to kafka topic ${TOPIC}"
 
 if [ ! -z ${SLEEP_FOREVER} ] ; then
     while true; do sleep 86400; done
--- e2e/kafka/scripts/wait_for_topic.sh
+++ e2e/kafka/scripts/wait_for_topic.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-until [[ $(sleep 2s && kafka-topics.sh --describe --topic ${TOPIC} --zookeeper zookeeper:2181) ]];
+ZOOKEEPER=${ZOOKEEPER:=zookeeper:2181}
+
+until [[ $(sleep 2s && kafka-topics.sh --describe --topic ${TOPIC} --zookeeper ${ZOOKEEPER}) ]];
 do
   echo "Waiting for Kafka topic ${TOPIC} to be ready..."
 done
