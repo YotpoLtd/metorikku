c3a9ca2689d82a54b3dd12a75ff27e3986b7e03e
lyogev <lyogev@yotpo.com>
2019-02-12 15:47:43 +0200

    feat(step): allow ignoring a failed step

--- src/main/scala/com/yotpo/metorikku/configuration/metric/Step.scala
+++ src/main/scala/com/yotpo/metorikku/configuration/metric/Step.scala
@@ -3,4 +3,8 @@ package com.yotpo.metorikku.configuration.metric
 case class Step(sql: Option[String],
                 file: Option[String],
                 classpath: Option[String],
-                dataFrameName: String)
+                dataFrameName: String,
+                var ignoreOnFailures: Option[Boolean]) {
+
+  ignoreOnFailures = Option(ignoreOnFailures.getOrElse(false))
+}
