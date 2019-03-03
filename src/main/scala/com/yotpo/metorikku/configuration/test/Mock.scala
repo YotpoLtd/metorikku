f4baea8a3e8d703ae4e0cf68094ed143a19cb2a8
lyogev <lyogev@yotpo.com>
2019-03-03 19:38:59 +0200

    fix(output): add the ability to use named outputs (#149)

--- src/main/scala/com/yotpo/metorikku/configuration/test/Mock.scala
+++ src/main/scala/com/yotpo/metorikku/configuration/test/Mock.scala
@@ -1,3 +1,5 @@
 package com.yotpo.metorikku.configuration.test
 
-case class Mock(name: String, path: String, streaming: Boolean = false)
+case class Mock(name: String, path: String, var streaming: Option[Boolean]) {
+  streaming = Option(streaming.getOrElse(false))
+}
--- src/main/scala/com/yotpo/metorikku/input/readers/file/FileInput.scala
+++ src/main/scala/com/yotpo/metorikku/input/readers/file/FileInput.scala
@@ -3,6 +3,14 @@ package com.yotpo.metorikku.input.readers.file
 import com.yotpo.metorikku.input.Reader
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
-case class FileInput(val name: String, path: String) extends Reader {
-  def read(sparkSession: SparkSession): DataFrame = FilesInput(name, Seq(path)).read(sparkSession)
+case class FileInput(val name: String,
+                     path: String,
+                     options: Option[Map[String, String]],
+                     schemaPath: Option[String],
+                     format: Option[String]) extends Reader {
+  def read(sparkSession: SparkSession): DataFrame = FilesInput(name,
+    path.split(","),
+    options,
+    schemaPath,
+    format).read(sparkSession)
 }
