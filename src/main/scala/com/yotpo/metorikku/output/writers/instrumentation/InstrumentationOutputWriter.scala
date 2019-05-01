e7c8a0283c402a693316d6b27508aaa27dfcf3ae
amitco1 <amit@yotpo.com>
2019-05-01 14:29:33 +0300

    feat(multitopic_writing): add watermark, add instrumentation writing with tags

--- src/main/scala/com/yotpo/metorikku/output/writers/instrumentation/InstrumentationOutputWriter.scala
+++ src/main/scala/com/yotpo/metorikku/output/writers/instrumentation/InstrumentationOutputWriter.scala
@@ -4,7 +4,6 @@ import com.yotpo.metorikku.exceptions.MetorikkuWriteFailedException
 import com.yotpo.metorikku.instrumentation.InstrumentationFactory
 import com.yotpo.metorikku.output.Writer
 import org.apache.log4j.{LogManager, Logger}
-import org.apache.spark.sql.types.StructField
 import org.apache.spark.sql.{DataFrame, Row}
 
 
@@ -14,17 +13,11 @@ class InstrumentationOutputWriter(props: Map[String, String],
                                   instrumentationFactory: InstrumentationFactory) extends Writer {
   @transient lazy val log: Logger = LogManager.getLogger(this.getClass)
 
-  // val keyColumnProperty: Option[String] = Option(props).getOrElse(Map()).get("keyColumn")
   val valueColumnProperty: Option[String] = Option(props).getOrElse(Map()).get("valueColumn")
   val timeColumnProperty: Option[String] = Option(props).getOrElse(Map()).get("timeColumn")
 
   override def write(dataFrame: DataFrame): Unit = {
     val columns = dataFrame.schema.fields.zipWithIndex
-    // val valueColumn or last
-    // val indexOfKeyCol = keyColumnProperty.flatMap(col => Option(dataFrame.schema.fieldNames.indexOf(col)))
-    if (columns.length == 2){
-      val indexOfValCol = 1
-    }
     val indexOfValCol = valueColumnProperty.flatMap(col => Option(dataFrame.schema.fieldNames.indexOf(col)))
     val indexOfTimeCol = timeColumnProperty.flatMap(col => Option(dataFrame.schema.fieldNames.indexOf(col)))
 
@@ -32,71 +25,36 @@ class InstrumentationOutputWriter(props: Map[String, String],
     dataFrame.foreachPartition(p => {
       val client = instrumentationFactory.create()
 
+      val actualIndexOfValCol = indexOfValCol.getOrElse(columns.length - 1)
+
       p.foreach(row => {
-        val tags = Map("metric" -> metricName, "dataframe" -> dataFrameName) ++
-          columns.filter{
-          case (column, index) => index != indexOfValCol
-        }.map{
-          case (column, index) =>
-            column.name -> row.get(index).asInstanceOf[AnyVal].toString
-        }.toMap
+        try {
 
+          val tags = Map("metric" -> metricName, "dataframe" -> dataFrameName) ++
+            columns.filter {
+              case (column, index) => index != actualIndexOfValCol || (!row.isNullAt(indexOfTimeCol.get) && index != indexOfValCol.get)
+            }.map {
+              case (column, index) =>
+                column.name -> row.get(index).asInstanceOf[AnyVal].toString
+            }.toMap
 
-        val valueOfValueColumn = row.get(indexOfValCol.getOrElse(null))
-        val longValue = valueOfValueColumn.asInstanceOf[Number].longValue()
-        val tags = Map("metric" -> metricName, "dataframe" -> dataFrameName) ++ keyColumnTags
-
-        if (valueOfRowAtCurrentCol != null && classOf[Number].isAssignableFrom(valueOfRowAtCurrentCol.getClass)) {
-          val longValue = valueOfRowAtCurrentCol.asInstanceOf[Number].longValue()
-          val keyColumnTags = getTagsForKeyColumn(indexOfKeyCol, row)
           val time = getTime(indexOfTimeCol, row)
-          val tags = Map("metric" -> metricName, "dataframe" -> dataFrameName) ++ keyColumnTags
+          val metricValue = row.get(actualIndexOfValCol)
 
-          client.gauge(name = column.name, value = longValue, tags = tags, time = time)
-        } else {
-          throw MetorikkuWriteFailedException("Value column doesn't contain a number")
-        }
-
-
-
-        for ((column, i) <- columns) {
-          try {
-            // Don't write key/time column to metric
-            if ((!indexOfKeyCol.isDefined || i != indexOfKeyCol.get) && (!indexOfTimeCol.isDefined || i != indexOfTimeCol.get)) {
-              val valueOfRowAtCurrentCol = row.get(i)
-              // Only if value is numeric
-              if (valueOfRowAtCurrentCol != null && classOf[Number].isAssignableFrom(valueOfRowAtCurrentCol.getClass)) {
-                val longValue = valueOfRowAtCurrentCol.asInstanceOf[Number].longValue()
-                val keyColumnTags = getTagsForKeyColumn(indexOfKeyCol, row)
-                val time = getTime(indexOfTimeCol, row)
-                val tags = Map("metric" -> metricName, "dataframe" -> dataFrameName) ++ keyColumnTags
-
-                client.gauge(name = column.name, value = longValue, tags = tags, time = time)
-              } else {
-                throw MetorikkuWriteFailedException("Value column doesn't contain a number")
-              }
-            }
-          } catch {
-            case ex: Throwable =>
-              throw MetorikkuWriteFailedException(s"failed to write instrumentation on data frame: $dataFrameName " +
-                s"for row: ${row.toString()} on column: ${column.name}", ex)
+          if (metricValue != null && classOf[Number].isAssignableFrom(metricValue.getClass)) {
+            val longValue = metricValue.asInstanceOf[Number].longValue()
+            val valueColumnName = row.schema.fieldNames(actualIndexOfValCol)
+            client.gauge(name = valueColumnName, value = longValue, tags = tags, time = time)
           }
+
+        }catch {
+          case ex: Throwable =>
+            throw MetorikkuWriteFailedException(s"failed to write instrumentation on data frame: $dataFrameName " +
+              s"for row: ${row.toString()}", ex)
         }
       })
-
       client.close()
     })
-  }
-
-  def getTagsForKeyColumn(indexOfKeyCol: Option[Int], row: Row): Map[String, String] = {
-    if (indexOfKeyCol.isDefined) {
-      if (!row.isNullAt(indexOfKeyCol.get)) {
-        return Map(keyColumnProperty.get -> row.get(indexOfKeyCol.get).asInstanceOf[AnyVal].toString)
-      } else {
-        throw MetorikkuWriteFailedException("Defined key column is null for row")
-      }
-    }
-    Map()
   }
 
   def getTime(indexOfTimeCol: Option[Int], row: Row): Long = {
