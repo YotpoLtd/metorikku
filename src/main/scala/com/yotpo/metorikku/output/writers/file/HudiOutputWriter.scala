11fbcc678bad0dca205b064277e6c97a03dde9aa
Irena <irenez753@gmail.com>
2019-09-03 20:05:53 +0300

    feat(stitch_marketo_whitelist): new hudi property: whitelist

--- src/main/scala/com/yotpo/metorikku/output/writers/file/HudiOutputWriter.scala
+++ src/main/scala/com/yotpo/metorikku/output/writers/file/HudiOutputWriter.scala
@@ -23,7 +23,8 @@ class HudiOutputWriter(props: Map[String, Object], hudiOutput: Option[Hudi]) ext
                                   tableName: Option[String],
                                   hivePartitions: Option[String],
                                   extraOptions: Option[Map[String, String]],
-                                  alignToPreviousSchema: Option[Boolean])
+                                  alignToPreviousSchema: Option[Boolean],
+                                  whitelist: Option[String])
 
   val hudiOutputProperties = HudiOutputProperties(
     props.get("path").asInstanceOf[Option[String]],
@@ -34,7 +35,8 @@ class HudiOutputWriter(props: Map[String, Object], hudiOutput: Option[Hudi]) ext
     props.get("tableName").asInstanceOf[Option[String]],
     props.get("hivePartitions").asInstanceOf[Option[String]],
     props.get("extraOptions").asInstanceOf[Option[Map[String, String]]],
-    props.get("alignToPreviousSchema").asInstanceOf[Option[Boolean]])
+    props.get("alignToPreviousSchema").asInstanceOf[Option[Boolean]],
+    props.get("whitelist").asInstanceOf[Option[String]])
 
 
   // scalastyle:off cyclomatic.complexity
@@ -46,6 +48,13 @@ class HudiOutputWriter(props: Map[String, Object], hudiOutput: Option[Hudi]) ext
     }
     log.info(s"Starting to write dataframe to hudi")
     var df = dataFrame
+
+    // Filter dataframe columns according to whitelist
+    df = this.hudiOutputProperties.whitelist match {
+      case Some(whitelist) => filterColumnsByWhitelist(df, whitelist)
+      case _ => df
+    }
+
     // To support schema evolution all fields should be nullable
     df = supportNullableFields(df)
 
@@ -261,6 +270,17 @@ class HudiOutputWriter(props: Map[String, Object], hudiOutput: Option[Hudi]) ext
       // scalastyle:on null
     }
     df
+  }
+
+  def filterColumnsByWhitelist(dataFrame: DataFrame, whitelist: String) : DataFrame = {
+    whitelist.trim.nonEmpty match {
+      case true => {
+        val wlColumns = whitelist.split(",").map(_.trim).toList.map(x => col(x))
+        dataFrame.select(wlColumns :_*)
+      }
+      case false => dataFrame
+    }
+
   }
 
 
