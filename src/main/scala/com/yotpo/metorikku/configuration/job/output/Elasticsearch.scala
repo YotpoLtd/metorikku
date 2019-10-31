fdcebf3eaec4f242ea3ac0237dba35a82ca04cea
wangqinghuan <1095193290@qq.com>
2019-10-31 09:06:23 +0800

    format

--- src/main/scala/com/yotpo/metorikku/configuration/job/output/Elasticsearch.scala
+++ src/main/scala/com/yotpo/metorikku/configuration/job/output/Elasticsearch.scala
@@ -1,6 +1,6 @@
 package com.yotpo.metorikku.configuration.job.output
 
-case class Elasticsearch(nodes: String,user: Option[String],password:Option[String])
+case class Elasticsearch(nodes: String, user: Option[String], password: Option[String])
 {
   require(Option(nodes).isDefined, "Elasticsearch connection: nodes is mandatory.")
 }
