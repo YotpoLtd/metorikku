57a930e37756790984b6f22a196dbeeddb47f77c
Administrator <1095193290@qq.com>
2019-12-02 18:24:59 +0800

    add mongoInput

---
+++ src/main/scala/com/yotpo/metorikku/configuration/job/input/MongoDB.scala
@@ -0,0 +1,17 @@
+package com.yotpo.metorikku.configuration.job.input
+
+import com.yotpo.metorikku.configuration.job.InputConfig
+import com.yotpo.metorikku.input.Reader
+import com.yotpo.metorikku.input.readers.elasticsearch.{ElasticsearchInput, MongoDBInput}
+
+case class MongoDB(uri: String,
+                   database: String,
+                   collection: String,
+                   options: Option[Map[String, String]]
+               ) extends InputConfig {
+  require(Option(uri).isDefined, "Mongo input: uri is mandatory")
+  require(Option(database).isDefined, "Mongo input: database is mandatory")
+  require(Option(collection).isDefined, "Mongo input: collection is mandatory")
+
+  override def getReader(name: String): Reader = MongoDBInput(name,uri,database,collection,options);
+}
