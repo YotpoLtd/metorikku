cc6ca4cb4ca1b51fcf79ca071a7ad1e4fecf6b5f
Nadav Uryan <>
2020-02-24 11:33:20 +0200

    fear(metorikku_spark): add exec commad to master and worker entry point to allow sigint when terminating

--- docker/spark/scripts/entrypoint-master.sh
+++ docker/spark/scripts/entrypoint-master.sh
@@ -10,5 +10,5 @@ SPARK_MASTER_HOST=${SPARK_MASTER_HOST:=spark-master}
 
 . "/spark/sbin/spark-config.sh"
 . "/spark/bin/load-spark-env.sh"
-/spark/sbin/../bin/spark-class org.apache.spark.deploy.master.Master \
+exec /spark/sbin/../bin/spark-class org.apache.spark.deploy.master.Master \
     --ip ${SPARK_MASTER_HOST} --port ${SPARK_MASTER_PORT} --webui-port ${SPARK_WEBUI_PORT}
