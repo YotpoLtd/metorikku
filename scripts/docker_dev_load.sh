9063124726630b6fc45499209c41d296e9e66874
lyogev <lyogev@yotpo.com>
2020-04-10 16:15:43 +0300

    fix(travis): refactor

--- scripts/docker_dev_load.sh
+++ scripts/docker_dev_load.sh
@@ -9,4 +9,4 @@ docker pull metorikku/dev:hive1-"$TRAVIS_COMMIT"
 docker tag metorikku/dev:spark-"$TRAVIS_COMMIT" metorikku/spark
 docker tag metorikku/dev:metorikku-"$TRAVIS_COMMIT" metorikku/metorikku
 docker tag metorikku/dev:hive-"$TRAVIS_COMMIT" metorikku/hive
-docker tag metorikku/dev:hive1-"$TRAVIS_COMMIT" metorikku/hive1
+docker tag metorikku/dev:hive1-"$TRAVIS_COMMIT" metorikku/hive:1
--- scripts/load_from_cache.sh
+++ scripts/load_from_cache.sh
@@ -5,5 +5,4 @@ echo "Loading metorikku JAR"
 cp -r $TARGET_CACHE/target .
 
 #./scripts/load_docker_from_cache.sh
-#./scripts/docker.sh
 ./scripts/docker_dev_load.sh
