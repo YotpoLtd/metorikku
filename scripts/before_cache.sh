e453316ab52466cf6ea83fe44b07376f39c2cf3d
lyogev <lyogev@yotpo.com>
2020-04-11 08:49:21 +0300

    fix(travis): refactor

--- scripts/before_cache.sh
+++ scripts/before_cache.sh
@@ -4,3 +4,4 @@ set -e
 find $HOME/.sbt -name "*.lock" | xargs rm
 find $HOME/.ivy2 -name "ivydata-*.properties" | xargs rm
 rm -f $HOME/.ivy2/.sbt.ivy.lock
+rm -f $HOME/.ivy2/cache/org.apache.hbase/hbase-server/ivy-1.1.1.xml
