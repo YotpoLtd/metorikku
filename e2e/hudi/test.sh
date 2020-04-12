37ddadb9e5daaa8b59f9d8ecbf86b98faf2a17c2
Irena <irenez753@gmail.com>
2020-04-12 21:06:03 +0300

    hudi_test_hive_sync

--- e2e/hudi/test.sh
+++ e2e/hudi/test.sh
@@ -6,6 +6,10 @@ set -e
 docker-compose up -d hive
 docker-compose up --exit-code-from spark-submit spark-submit
 docker-compose up --exit-code-from hive-tester hive-tester
+docker-compose up --exit-code-from spark-submit-manual-hive-sync spark-submit-manual-hive-sync
+docker-compose up --exit-code-from hive-tester-manual-hive-sync hive-tester-manual-hive-sync
+docker-compose up --exit-code-from spark-submit-manual-hive-sync-non-partition spark-submit-manual-hive-sync-non-partition
+docker-compose up --exit-code-from hive-tester-manual-hive-sync-no-partition hive-tester-manual-hive-sync-no-partition
 exit_code=$(docker ps -aq -f label=com.docker.compose.project=hudi | xargs -I{} docker inspect {} --format='{{.State.ExitCode}}' | paste -sd+ - | bc)
 docker-compose down
 exit $exit_code
