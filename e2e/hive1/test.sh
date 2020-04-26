857fc3170705d442394da7feaed9629c2733da8e
lyogev <lyogev@yotpo.com>
2020-04-26 19:36:20 +0300

    feat(hadoop): upgrade to hadoop 3.2.1

--- e2e/hive1/test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-docker-compose up -d hive-db
-docker-compose up --exit-code-from spark-submit spark-submit
-docker-compose up --exit-code-from hive-tester hive-tester
-exit_code=$(docker ps -aq -f label=com.docker.compose.project=hive | xargs -I{} docker inspect {} --format='{{.State.ExitCode}}' | paste -sd+ - | bc)
-docker-compose down
-exit $exit_code
--- scripts/docker.sh
+++ scripts/docker.sh
@@ -7,10 +7,9 @@ docker pull metorikku/spark:latest
 docker pull metorikku/metorikku:latest
 docker pull $(grep -ioP '(?<=^from)\s+\S+' docker/hive/Dockerfile)
 docker pull metorikku/hive:latest
-docker pull $(grep -ioP '(?<=^from)\s+\S+' docker/hive1/Dockerfile)
-docker pull metorikku/hive:1
+
+(cd docker/spark/hive-shim-loader && mvn install)
 
 docker build -t metorikku/spark --cache-from metorikku/spark:latest --build-arg SPARK_VERSION=$SPARK_VERSION --build-arg HADOOP_VERSION=$HADOOP_VERSION -f docker/spark/Dockerfile docker/spark
-docker build -t metorikku/hive:1 --cache-from metorikku/hive:1 --build-arg HIVE_VERSION=$HIVE1_VERSION --build-arg HUDI_HIVE1_VERSION=$HUDI_HIVE1_VERSION -f docker/hive1/Dockerfile docker/hive1
 docker build -t metorikku/hive --cache-from metorikku/hive --build-arg HIVE_VERSION=$HIVE_VERSION --build-arg HUDI_VERSION=$HUDI_VERSION -f docker/hive/Dockerfile docker/hive
 docker build -t metorikku/metorikku -f docker/metorikku/Dockerfile .
