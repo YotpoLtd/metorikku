c7aeab99163e75e7d9298d58806383634a6b1c4d
Doe-Ed <63307024+Doe-Ed@users.noreply.github.com>
2020-05-18 17:13:57 +0300

    fix(SelectiveMerge):always override with df2's column (if exists) (#322)
    * fix(SelectiveMerge):always override with df2's column (if exists)
    
    * fix(SelectiveMerge):Favor not favour

--- src/main/scala/com/yotpo/metorikku/code/steps/SelectiveMerge.scala
+++ src/main/scala/com/yotpo/metorikku/code/steps/SelectiveMerge.scala
@@ -5,14 +5,13 @@ import org.apache.log4j.{LogManager, Logger}
 import org.apache.spark.sql.catalyst.expressions.NamedExpression
 import org.apache.spark.sql.{Column, DataFrame}
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.types.StructType
 
 object SelectiveMerge {
   private val message = "You need to send 3 parameters with the names of the dataframes to merge and the key(s) to merge on" +
-    "(merged df1 into df2 favouring values from df2): df1, df2, Seq[String]"
+    "(merged df1 into df2 favoring values from df2): df1, df2, Seq[String]"
   private val log: Logger = LogManager.getLogger(this.getClass)
-  private val colRenamePrefix = "df2_"
-  private val colRenamePrefixLen = colRenamePrefix.length
+  private val colRenameSuffixLength = 10000 // (5 digits)
+  private val colRenamePrefix = scala.util.Random.nextInt(colRenameSuffixLength).toString
   private class InputMatcher[K](ks: K*) {
     def unapplySeq[V](m: Map[K, V]): Option[Seq[V]] = if (ks.forall(m.contains)) Some(ks.map(m)) else None
   }
@@ -37,11 +36,25 @@ object SelectiveMerge {
           df1.createOrReplaceTempView(dataFrameName)
         }
         else {
+          logOverrides(df1, df2, joinKeys)
           merge(df1, df2, joinKeys).createOrReplaceTempView(dataFrameName)
         }
       }
       case _ => throw MetorikkuException(message)
     }
+  }
+
+  def logOverrides(df1: DataFrame, df2: DataFrame, joinKeys: Seq[String]): Unit = {
+    val df1SchemaTitles = df1.schema.map(f => f.name).toList
+    val df2SchemaTitles = df2.schema.map(f => f.name).toList
+
+    val overridenColumns = df2SchemaTitles.filter(p => df1SchemaTitles.contains(p) && !joinKeys.contains(p))
+    val df1OnlyColumns = df1SchemaTitles diff df2SchemaTitles
+    val df2OnlyColumns = df2SchemaTitles diff df1SchemaTitles
+
+    log.info("DF1 columns which will be overriden: " + overridenColumns)
+    log.info("DF1 columns which are not found in DF2: " + df1OnlyColumns)
+    log.info("DF2 columns which are not found in DF1: " + df2OnlyColumns)
   }
 
   def merge(df1: DataFrame, df2: DataFrame, joinKeys: Seq[String]): DataFrame = {
@@ -62,17 +75,6 @@ object SelectiveMerge {
     ).join(df1, joinKeys,"outer")
   }
 
-  def isMemberOfDf1(schema: StructType, name: String): Boolean = {
-    val schemaNames = schema.map(f => f.name)
-
-    if (name.startsWith(colRenamePrefix)) {
-      schemaNames.contains(name.substring(colRenamePrefixLen))
-    }
-    else {
-      false
-    }
-  }
-
   def getMergedSchema(df1: DataFrame, df2: DataFrame, joinKeys: Seq[String]): Seq[Column] =  {
     val mergedSchemaNames = (df1.schema.map(f => f.name) ++ df2.schema.map(f => f.name)).distinct
 
@@ -88,36 +90,30 @@ object SelectiveMerge {
     mergedSchema
   }
 
-
   def overrideConflictingValues(df1: DataFrame, df2: DataFrame, mergedDf: DataFrame, joinKeys: Seq[String]): DataFrame = {
     val mergedSchema = getMergedSchema(df1, df2, joinKeys)
 
-    val mergedDfBuilder = mergedDf.select(
+    mergedDf.select(
       mergedSchema.map{
         case (currColumn: Column) => {
           val colName = currColumn.expr.asInstanceOf[NamedExpression].name
-          // Column is a part of df1 and doesn't belong to the join keys.
-          if (isMemberOfDf1(df1.schema, colName) && !joinKeys.contains(colName)) {
-            val origColName = colName.substring(colRenamePrefixLen)
-            when(mergedDf(colName).isNotNull, mergedDf(colName).cast(df1.schema(origColName).dataType))
-              .otherwise(df1(origColName))
-              .alias(origColName)
+          val colNameArr = colName.split(colRenamePrefix)
+          val colNameOrig = if (colNameArr.size > 1) colNameArr(1) else colName
+
+          // Belongs to DF2, override.
+          if (colNameArr.size > 1) {
+            mergedDf(colName).alias(colNameOrig)
           }
-          // Column doesn't belong to df1 or is join key
+          // Is the join key(s)
+          else if (joinKeys.contains(colName)) {
+            mergedDf(colName)
+          }
+          // Only exists in DF1.
           else {
-            // Column doesn't belong to df1
-            if (colName.startsWith(colRenamePrefix)) {
-              currColumn.alias(colName.substring(colRenamePrefixLen))
-            }
-            // Column is the merge key
-            else {
-              currColumn
-            }
+            df1(colName)
           }
         }
       }: _*
     )
-
-    mergedDfBuilder
   }
 }
