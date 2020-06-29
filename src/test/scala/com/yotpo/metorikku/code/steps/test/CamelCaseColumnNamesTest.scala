a87d23f37a823067a6b83a315327a871250bb783
Ron Barabash <rbarabash@yotpo.com>
2020-06-29 13:44:33 +0300

    feat(camel-case-column-names): added a udf to convert all column names to camelCase

---
+++ src/test/scala/com/yotpo/metorikku/code/steps/test/CamelCaseColumnNamesTest.scala
@@ -0,0 +1,42 @@
+package com.yotpo.metorikku.code.steps.test
+
+import com.holdenkarau.spark.testing.DataFrameSuiteBase
+import com.yotpo.metorikku.code.steps.CamelCaseColumnNames
+import org.apache.log4j.{Level, LogManager, Logger}
+import org.apache.spark.sql.{SQLContext, SparkSession}
+import org.scalatest.{FunSuite, _}
+
+//noinspection ScalaStyle
+class CamelCaseColumnNamesTest extends FunSuite with DataFrameSuiteBase with BeforeAndAfterEach {
+  private val log: Logger = LogManager.getLogger(this.getClass)
+  private var sparkSession: SparkSession = _
+  Logger.getLogger("org").setLevel(Level.WARN)
+
+  override def beforeEach() {
+    sparkSession = SparkSession.builder().appName("udf tests")
+      .master("local")
+      .config("", "")
+      .getOrCreate()
+  }
+
+  test("CamelCaseColumnNames") {
+    val sparkSession = SparkSession.builder.appName("test").getOrCreate()
+    val sqlContext = new SQLContext(sparkSession.sparkContext)
+    import sqlContext.implicits._
+
+    val employeeData = Seq(
+      ("James", 1),
+      ("Maria", 2)
+    )
+    employeeData.toDF("employee_name", "employee_salary").createOrReplaceTempView("employeeDataActual")
+    employeeData.toDF("employeeName", "employeeSalary").createOrReplaceTempView("employeeDataExpected")
+    CamelCaseColumnNames.run(sparkSession, "", "employeeDataExpectedResult", Some(Map("table" -> "employeeDataActual")))
+
+    assertDataFrameEquals(sparkSession.table("employeeDataExpected"), sparkSession.table("employeeDataExpectedResult"))
+
+  }
+
+  override def afterEach() {
+    sparkSession.stop()
+  }
+}
