a47c91af85c3f4895d20da787dfb862957eb86fe
Ron Barabash <rbarabash@yotpo.com>
2020-07-26 11:43:11 +0300

    feat(Dockerfile): push k8s-spark-operator docker

--- scripts/docker_load_dev.sh
+++ scripts/docker_load_dev.sh
@@ -5,10 +5,12 @@ tag=$(([ ! "${TRAVIS_PULL_REQUEST}" = "false" ] && echo "${TRAVIS_PULL_REQUEST}"
 
 docker pull metorikku/dev:metorikku_$tag
 docker pull metorikku/dev:spark_$tag
+docker pull metorikku/dev:k8s-spark-operator_$tag
 docker pull metorikku/dev:hive_$tag
 docker pull metorikku/dev:hive1_$tag
 
 docker tag metorikku/dev:metorikku_$tag metorikku/metorikku
 docker tag metorikku/dev:spark_$tag metorikku/spark
+docker tag metorikku/dev:k8s-spark-operator_$tag metorikku/k8s-spark-operator
 docker tag metorikku/dev:hive_$tag metorikku/hive
 docker tag metorikku/dev:hive1_$tag metorikku/hive:1
