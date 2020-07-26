f89343a33583924e2b073cdbfcd203270698546e
Ron Barabash <rbarabash@yotpo.com>
2020-07-26 12:10:11 +0300

    feat(Dockerfile): fixes

--- scripts/docker_publish.sh
+++ scripts/docker_publish.sh
@@ -4,7 +4,7 @@ set -e
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker tag metorikku/metorikku metorikku/metorikku:"$TRAVIS_TAG"_spark_"$SPARK_VERSION"
 docker tag metorikku/spark metorikku/spark:$SPARK_VERSION
-docker tag metorikku/k8s-spark-operator metorikku/k8s-spark-operator:$ SPARK_VERSION
+docker tag metorikku/k8s-spark-operator metorikku/k8s-spark-operator:$SPARK_VERSION
 docker tag metorikku/hive metorikku/hive:$HIVE_VERSION
 docker tag metorikku/hive:1 metorikku/hive:$HIVE1_VERSION
 
