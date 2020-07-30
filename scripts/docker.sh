7f7db2e16df70452cf9dc3dac4750ce0989770c9
Shir <sbromberg@yotpo.com>
2020-07-30 12:05:51 +0300

    feat(spark-docker): run from root folder

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -14,7 +14,7 @@ docker pull metorikku/hive:1
 echo "RON"
 pwd
 docker build -t metorikku/spark --cache-from metorikku/spark:latest --build-arg SPARK_VERSION=$SPARK_VERSION --build-arg HADOOP_VERSION=$HADOOP_VERSION -f docker/spark/Dockerfile docker/spark
-docker build -t metorikku/k8s-spark-operator --cache-from metorikku/k8s-spark-operator --build-arg SPARK_VERSION=$SPARK_VERSION --build-arg HADOOP_VERSION=$HADOOP_VERSION -f docker/k8s-spark-operator/Dockerfile docker/k8s-spark-operator
+docker build -t metorikku/k8s-spark-operator --cache-from metorikku/k8s-spark-operator --build-arg SPARK_VERSION=$SPARK_VERSION --build-arg HADOOP_VERSION=$HADOOP_VERSION -f docker/k8s-spark-operator/Dockerfile .
 docker build -t metorikku/hive:1 --cache-from metorikku/hive:1 --build-arg HIVE_VERSION=$HIVE1_VERSION --build-arg HUDI_HIVE1_VERSION=$HUDI_HIVE1_VERSION -f docker/hive1/Dockerfile docker/hive1
 docker build -t metorikku/hive --cache-from metorikku/hive --build-arg HIVE_VERSION=$HIVE_VERSION --build-arg HUDI_VERSION=$HUDI_VERSION -f docker/hive/Dockerfile docker/hive
 docker build -t metorikku/metorikku -f docker/metorikku/Dockerfile .
