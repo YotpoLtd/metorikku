2a43bf1765c397dad643bc8fb2066eb62145d8ef
lyogev <lyogev@yotpo.com>
2020-10-01 09:05:12 +0300

    fix(build): publish to ECR

--- scripts/ecr_publish.sh
+++ scripts/ecr_publish.sh
@@ -13,10 +13,10 @@ docker tag $ECR_PATH/metorikku:standalone $ECR_PATH/metorikku:standalone_"$TRAVI
 docker tag $ECR_PATH/metorikku:spark2_standalone $ECR_PATH/metorikku:standalone_"$TRAVIS_TAG"_spark_"$SPARK2_VERSION"
 docker tag $ECR_PATH/metorikku:k8s $ECR_PATH/metorikku:k8s_"$TRAVIS_TAG"_spark_"$SPARK_VERSION"
 docker tag $ECR_PATH/metorikku:spark2_k8s $ECR_PATH/metorikku:k8s_"$TRAVIS_TAG"_spark_"$SPARK_VERSION"
-docker tag $ECR_PATH/hive $ECR_PATH/hive:$HIVE_VERSION
+docker tag metorikku/hive $ECR_PATH/hive:hive_$HIVE_VERSION
 
 docker push $ECR_PATH/metorikku:standalone
 docker push $ECR_PATH/metorikku:spark2_standalone
 docker push $ECR_PATH/metorikku:k8s
 docker push $ECR_PATH/metorikku:spark2_k8s
-docker push $ECR_PATH/hive
+docker push $ECR_PATH/hive:hive_$HIVE_VERSION
