b46afce04887c54b53b7f68dbd61132d6899a08f
Assaf Liebstein <aliebstein@yotpo.com>
2021-02-07 09:33:47 +0200

    feat(DqChecks):add is contained check

--- src/main/scala/com/yotpo/metorikku/metric/stepActions/dataQuality/DataQualityCheck.scala
+++ src/main/scala/com/yotpo/metorikku/metric/stepActions/dataQuality/DataQualityCheck.scala
@@ -1,19 +1,20 @@
 package com.yotpo.metorikku.metric.stepActions.dataQuality
 
 import com.amazon.deequ.checks.Check
-import com.yotpo.metorikku.metric.stepActions.dataQuality.operators.{HasSize, HasUniqueness, IsComplete, IsUnique}
+import com.yotpo.metorikku.metric.stepActions.dataQuality.operators.{HasSize, HasUniqueness, IsComplete, IsUnique, IsContainedIn}
 import org.apache.log4j.LogManager
 
 case class DataQualityCheck(
                              isComplete: Option[IsComplete] = None,
                              isUnique: Option[IsUnique] = None,
                              hasSize: Option[HasSize] = None,
-                             hasUniqueness: Option[HasUniqueness] = None
+                             hasUniqueness: Option[HasUniqueness] = None,
+                             isContainedIn: Option[IsContainedIn] = None
                            ) {
   private val log = LogManager.getLogger(this.getClass)
 
   def getCheck(level: String): Check = {
-    val operator = Seq(isComplete, isUnique, hasSize, hasUniqueness).find(
+    val operator = Seq(isComplete, isUnique, hasSize, hasUniqueness, isContainedIn).find(
       x => x.isDefined
     ).get.get
     try {
