4c6a153148367c84924ab934793049f7a7a2a947
Liran Yogev <23477645+lyogev@users.noreply.github.com>
2021-02-11 15:36:17 +0200

    fix(docker): fix docker k8s spark2 image (#401)
    * fix(docker): fix docker k8s spark2 image
    
    * fix(scala): scala version should be 2.12.10 for 2.12
    
    Co-authored-by: Ron <rbarabash@yotpo.com>

--- scripts/docker_publish.sh
+++ scripts/docker_publish.sh
@@ -5,7 +5,7 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker tag metorikku/metorikku:standalone metorikku/metorikku:standalone_"$TRAVIS_TAG"_spark_"$SPARK_VERSION"
 docker tag metorikku/metorikku:spark2_standalone metorikku/metorikku:standalone_"$TRAVIS_TAG"_spark_"$SPARK2_VERSION"
 docker tag metorikku/metorikku:k8s metorikku/metorikku:k8s_"$TRAVIS_TAG"_spark_"$SPARK_VERSION"
-docker tag metorikku/metorikku:spark2_k8s metorikku/metorikku:k8s_"$TRAVIS_TAG"_spark_"$SPARK_VERSION"
+docker tag metorikku/metorikku:spark2_k8s metorikku/metorikku:k8s_"$TRAVIS_TAG"_spark_"$SPARK2_VERSION"
 docker tag metorikku/hive metorikku/hive:$HIVE_VERSION
 
 docker push metorikku/metorikku:standalone
@@ -15,6 +15,6 @@ docker push metorikku/metorikku:standalone_"$TRAVIS_TAG"_spark_"$SPARK2_VERSION"
 docker push metorikku/metorikku:k8s
 docker push metorikku/metorikku:k8s_"$TRAVIS_TAG"_spark_"$SPARK_VERSION"
 docker push metorikku/metorikku:spark2_k8s
-docker push metorikku/metorikku:k8s_"$TRAVIS_TAG"_spark_"$SPARK_VERSION"
+docker push metorikku/metorikku:k8s_"$TRAVIS_TAG"_spark_"$SPARK2_VERSION"
 docker push metorikku/hive
 docker push metorikku/hive:$HIVE_VERSION
