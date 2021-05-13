e63ef58d99f83d061942b2139bfa99a13bcb7bd4
ahalatzi <ahalatzi@yotpo.com>
2021-05-13 09:19:24 +0300

    use [String,Any] for writers' property bag

--- src/main/scala/com/yotpo/metorikku/output/writers/file/ParquetOutputWriter.scala
+++ src/main/scala/com/yotpo/metorikku/output/writers/file/ParquetOutputWriter.scala
@@ -2,5 +2,5 @@ package com.yotpo.metorikku.output.writers.file
 
 import com.yotpo.metorikku.configuration.job.output.File
 
-class ParquetOutputWriter(props: Map[String, String], outputFile: Option[File])
+class ParquetOutputWriter(props: Map[String, Any], outputFile: Option[File])
   extends FileOutputWriter(Option(props).getOrElse(Map()) + ("format" -> "parquet"), outputFile)
