08d0903d40a08b2df5d971e1374b0f54b2a33c32
Assaf Liebstein <aliebstein@yotpo.com>
2021-06-15 11:09:43 +0300

    feat(hive):change health check (#437)

--- docker/hive/health-check.sh
+++ docker/hive/health-check.sh
@@ -4,4 +4,4 @@ set -ev
 HIVE_SERVER_PORT=${HIVE_SERVER_PORT:=10000}
 
 echo Health check for Hive:
-/opt/hive/bin/beeline -u "jdbc:hive2://127.0.0.1:${HIVE_SERVER_PORT}/default;auth=noSasl" -n health_check -e "show tables;"
+/opt/hive/bin/beeline -u "jdbc:hive2://127.0.0.1:${HIVE_SERVER_PORT}/default;auth=noSasl" -n health_check -e "show databases;"
