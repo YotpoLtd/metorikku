3eb6952dc4bc937b0cf7d1271d30eafad4324ec3
raphael.luta <raphael.luta@ALFR-933J7Y2-L.eu.corp.airliquide.com>
2021-06-21 17:22:53 +0200

    Add extended JDBC options in output configuration

--- src/main/scala/com/yotpo/metorikku/configuration/job/output/JDBC.scala
+++ src/main/scala/com/yotpo/metorikku/configuration/job/output/JDBC.scala
@@ -3,7 +3,12 @@ package com.yotpo.metorikku.configuration.job.output
 case class JDBC(connectionUrl: String,
                 user: String,
                 password: String,
-                driver: String
+                driver: String,
+                sessionInitStatement: Option[String],
+                truncate: Option[String],
+                cascadeTruncate: Option[String],
+                createTableOptions: Option[String],
+                createTableColumnTypes: Option[String]
                ) {
   require(Option(connectionUrl).isDefined, "JDBC connection: connection url is mandatory")
   require(Option(user).isDefined, "JDBC connection: user is mandatory")
