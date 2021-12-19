8eae1862a1aa3511c5645afba340889f55d669e0
ahalatzi <ahalatzi@yotpo.com>
2021-12-19 11:23:36 +0200

    feat(hudi): update hudi to latest version (0.10.0)

--- src/main/scala/com/yotpo/metorikku/utils/HudiUtils.scala
+++ src/main/scala/com/yotpo/metorikku/utils/HudiUtils.scala
@@ -1,14 +1,12 @@
 package com.yotpo.metorikku.utils
 
 import org.apache.hudi.avro.model.HoodieCompactionPlan
-import org.apache.hudi.common.table.timeline.HoodieInstant
+import org.apache.hudi.common.table.HoodieTableMetaClient
 import org.apache.hudi.common.table.timeline.HoodieInstant.State
-import org.apache.hudi.common.table.{HoodieTableMetaClient, HoodieTimeline}
+import org.apache.hudi.common.table.timeline.{HoodieInstant, HoodieTimeline}
 import org.apache.hudi.common.util.CompactionUtils
 import org.apache.hudi.common.util.collection.ImmutablePair
-import org.apache.hudi.config.HoodieWriteConfig
 import org.apache.hudi.exception.TableNotFoundException
-import org.apache.hudi.table.HoodieTable
 import org.apache.log4j.LogManager
 import org.apache.spark.SparkContext
 import org.apache.spark.api.java.JavaSparkContext
@@ -17,12 +15,14 @@ object HudiUtils {
   val log = LogManager.getLogger(this.getClass)
   def deletePendingCompactions(sparkContext: SparkContext, basePath: String): Unit = {
     try {
-      val jsc = JavaSparkContext.fromSparkContext(sparkContext)
-      val hudiMetaclient = new HoodieTableMetaClient(sparkContext.hadoopConfiguration, basePath)
-      val writerConfig = HoodieWriteConfig.newBuilder().withPath(basePath).build()
-      val hudiTable = HoodieTable.getHoodieTable(hudiMetaclient, writerConfig, jsc)
-      val pendingCompactionPlans = CompactionUtils.getAllPendingCompactionPlans(hudiMetaclient)
-      val activeTimeline = hudiTable.getActiveTimeline()
+//      val jsc = JavaSparkContext.fromSparkContext(sparkContext)
+      val hudiMetaClient = HoodieTableMetaClient.builder.setConf(sparkContext.hadoopConfiguration)
+                                                        .setBasePath(basePath)
+                                                        .build()
+//      val writerConfig = HoodieWriteConfig.newBuilder().withPath(basePath).build()
+//      val hudiTable = HoodieTable.getHoodieTable(hudiMetaClient, writerConfig, jsc)
+      val pendingCompactionPlans = CompactionUtils.getAllPendingCompactionPlans(hudiMetaClient)
+      val activeTimeline = hudiMetaClient.getActiveTimeline()
 
       pendingCompactionPlans.toArray().foreach({ pendingCompactionPlan => {
         val inflightInstant = pendingCompactionPlan.asInstanceOf[ImmutablePair[HoodieInstant, HoodieCompactionPlan]].getLeft
