8fac7a0d407907abf84861e748e2d8838f798ef3
ahalatzi <ahalatzi@yotpo.com>
2021-12-22 23:39:50 +0200

    feat(hoodie): update to 0.10.0

--- src/main/scala/com/yotpo/metorikku/utils/HudiUtils.scala
+++ src/main/scala/com/yotpo/metorikku/utils/HudiUtils.scala
@@ -7,28 +7,29 @@ import org.apache.hudi.common.table.timeline.{HoodieInstant, HoodieTimeline}
 import org.apache.hudi.common.util.CompactionUtils
 import org.apache.hudi.common.util.collection.ImmutablePair
 import org.apache.hudi.exception.TableNotFoundException
-import org.apache.log4j.LogManager
+import org.apache.log4j.{LogManager, Logger}
 import org.apache.spark.SparkContext
-import org.apache.spark.api.java.JavaSparkContext
 
 object HudiUtils {
-  val log = LogManager.getLogger(this.getClass)
+  val log: Logger = LogManager.getLogger(this.getClass)
+
   def deletePendingCompactions(sparkContext: SparkContext, basePath: String): Unit = {
     try {
-//      val jsc = JavaSparkContext.fromSparkContext(sparkContext)
-      val hudiMetaClient = HoodieTableMetaClient.builder.setConf(sparkContext.hadoopConfiguration)
-                                                        .setBasePath(basePath)
-                                                        .build()
-//      val writerConfig = HoodieWriteConfig.newBuilder().withPath(basePath).build()
-//      val hudiTable = HoodieTable.getHoodieTable(hudiMetaClient, writerConfig, jsc)
+
+      val hudiMetaClient = HoodieTableMetaClient
+        .builder
+        .setConf(sparkContext.hadoopConfiguration)
+        .setBasePath(basePath)
+        .build()
+
       val pendingCompactionPlans = CompactionUtils.getAllPendingCompactionPlans(hudiMetaClient)
-      val activeTimeline = hudiMetaClient.getActiveTimeline()
+      val activeTimeline = hudiMetaClient.getActiveTimeline
 
       pendingCompactionPlans.toArray().foreach({ pendingCompactionPlan => {
         val inflightInstant = pendingCompactionPlan.asInstanceOf[ImmutablePair[HoodieInstant, HoodieCompactionPlan]].getLeft
         log.info(s"Deleting pending inflight compaction: ${inflightInstant.getFileName}")
         activeTimeline.deleteInflight(inflightInstant)
-        val compactionRequestedInstant = new HoodieInstant(State.REQUESTED, HoodieTimeline.COMPACTION_ACTION, inflightInstant.getTimestamp);
+        val compactionRequestedInstant = new HoodieInstant(State.REQUESTED, HoodieTimeline.COMPACTION_ACTION, inflightInstant.getTimestamp)
         log.info(s"Deleting pending compaction requested: ${compactionRequestedInstant.getFileName}")
         activeTimeline.deleteCompactionRequested(compactionRequestedInstant)
       }
