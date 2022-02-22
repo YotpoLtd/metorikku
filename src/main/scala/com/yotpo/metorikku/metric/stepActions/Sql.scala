200923665afed748e6a46db79ece3f8e1463442b
ahalatzi <ahalatzi@yotpo.com>
2022-02-22 22:22:05 +0200

    feat: support checkpoint

--- src/main/scala/com/yotpo/metorikku/metric/stepActions/Sql.scala
+++ src/main/scala/com/yotpo/metorikku/metric/stepActions/Sql.scala
@@ -3,15 +3,16 @@ package com.yotpo.metorikku.metric.stepActions
 import com.yotpo.metorikku.metric.StepAction
 import com.yotpo.metorikku.metric.stepActions.dataQuality.DataQualityCheckList
 import org.apache.log4j.LogManager
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 
 /**
-  * Represents the SQL query to run
-  */
+ * Represents the SQL query to run
+ */
 case class Sql(query: String, dataFrameName: String, showPreviewLines: Int,
                cacheOnPreview: Option[Boolean],
                showQuery: Option[Boolean],
-               dq: Option[DataQualityCheckList]
+               dq: Option[DataQualityCheckList],
+               checkpoint: Option[Boolean]
               ) extends StepAction[DataFrame] {
   val log = LogManager.getLogger(this.getClass)
 
@@ -21,11 +22,26 @@ case class Sql(query: String, dataFrameName: String, showPreviewLines: Int,
       case _ =>
     }
 
-    val newDf = sparkSession.sqlContext.sql(query)
+    val newDf = checkpointDataframe(dataFrameName, sparkSession.sqlContext.sql(query), checkpoint)
+
     newDf.createOrReplaceTempView(dataFrameName)
     printStep(newDf, dataFrameName)
     runDQValidation(sparkSession, dataFrameName, dq)
     newDf
+  }
+
+  private def checkpointDataframe(dataFrameName: String, df: DataFrame, checkpoint: Option[Boolean]): Dataset[Row] = {
+    checkpoint match {
+      case Some(true) =>
+        df.sparkSession.sparkContext.getCheckpointDir match {
+          case None => {
+            log.warn(s"Did not commit checkpoint for data frame ${dataFrameName}. No checkpoint path was specified")
+            df
+          }
+          case _ => df.checkpoint()
+        }
+      case _ => df
+    }
   }
 
   private def printStep(stepResult: DataFrame, stepName: String): Unit = {
