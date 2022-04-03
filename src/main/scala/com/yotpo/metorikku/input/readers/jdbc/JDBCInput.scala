b1e3d09a16083ce746e9e7dd3aa97f8fad701ec9
golankiviti <golkiv@gmail.com>
2022-04-03 14:06:17 +0300

    feat(add parameters to mongo loader and jdbc loader) (#477)
    MongoDB & JDBC inputs extra params

--- src/main/scala/com/yotpo/metorikku/input/readers/jdbc/JDBCInput.scala
+++ src/main/scala/com/yotpo/metorikku/input/readers/jdbc/JDBCInput.scala
@@ -3,20 +3,52 @@ package com.yotpo.metorikku.input.readers.jdbc
 import com.yotpo.metorikku.input.Reader
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
-case class JDBCInput(val name: String, connectionUrl: String, user: String,
-                     password: String, table: String,
+case class JDBCInput(val name: String,
+                     connectionUrl: String,
+                     user: String,
+                     password: String,
+                     table: String,
+                     partitionsNumber: Long = 0,
+                     partitionColumn: String = "id",
                      options: Option[Map[String, String]]) extends Reader {
+  val MinNumberOfPartitions = 10
+  val MaxNumberOfPartitions = 640
+  val TableSizeFactor = 1000000
+
   def read(sparkSession: SparkSession): DataFrame = {
     val url = connectionUrl
     val baseDBOptions = Map(
       "url" -> url,
       "user" -> user,
       "password" -> password,
-      "dbTable" -> table)
+      "dbTable" -> table
+    )
 
-    val DBOptions = baseDBOptions ++ options.getOrElse(Map())
+    val extraOptions = {
+      val tableInfo = sparkSession.read.format("jdbc").options(baseDBOptions)
+        .option("dbTable", s"(SELECT max(${partitionColumn}) AS maxId FROM $table) tmp")
+        .load
+        .collect()(0)
 
-    val dbTable = sparkSession.read.format("jdbc").options(DBOptions)
+      val maxId = if (tableInfo(0) == null) 0 else tableInfo(0).toString.toLong
+
+      val numPartitions = partitionsNumber match {
+        case 0 => (maxId / TableSizeFactor).max(MinNumberOfPartitions).min(MaxNumberOfPartitions)
+        case _ => partitionsNumber
+      }
+
+      Map(
+        "partitionColumn" -> partitionColumn,
+        "lowerBound" -> "0",
+        "upperBound" -> maxId.toString,
+        "numPartitions" -> numPartitions.toString
+      )
+    }
+
+    val dbOptions = baseDBOptions ++ options.getOrElse(Map()) ++ extraOptions
+
+    val dbTable = sparkSession.read.format("jdbc").options(dbOptions)
+
     dbTable.load()
   }
 }
--- src/main/scala/com/yotpo/metorikku/input/readers/mongodb/MongoDBInput.scala
+++ src/main/scala/com/yotpo/metorikku/input/readers/mongodb/MongoDBInput.scala
@@ -1,16 +1,85 @@
-package com.yotpo.metorikku.input.readers.elasticsearch
+package com.yotpo.metorikku.input.readers.mongodb
 
+import com.mongodb.spark.MongoSpark
+import com.mongodb.spark.config.ReadConfig
 import com.yotpo.metorikku.input.Reader
 import org.apache.spark.sql.{DataFrame, SparkSession}
+import com.yotpo.metorikku.input.readers.file.SchemaConverter
+import com.yotpo.metorikku.utils.FileUtils
+import com.mongodb.spark.sql._
+import com.yotpo.metorikku.input.readers.mongodb.MongoDBInput.buildDf
+import org.apache.spark.sql.types.{ArrayType, StringType, StructField, StructType}
+import scala.collection.mutable
 
-case class MongoDBInput(name: String, uri: String, database: String, collection: String,
-                     options: Option[Map[String, String]]) extends Reader {
+case class MongoDBInput(name: String,
+                        uri: String,
+                        database: String,
+                        collection: String,
+                        sampleSize: String = "10000",
+                        partitionKey: String = "_id",
+                        samplesPerPartition: String = "200",
+                        schemaPath: Option[String] = None,
+                        options: Option[Map[String, String]]) extends Reader {
   def read(sparkSession: SparkSession): DataFrame = {
-    var mongoDBOptions = Map("uri" -> uri, "database" -> database, "collection" -> collection)
+    var mongoDBOptions = Map(
+      "uri" -> uri,
+      "database" -> database,
+      "collection" -> collection,
+      "partitioner" -> "MongoSamplePartitioner",
+      "sampleSize" -> sampleSize,
+      "partitionerOptions.samplesPerPartition" -> samplesPerPartition,
+      "partitionerOptions.partitionKey" -> partitionKey
+    )
 
     mongoDBOptions ++= options.getOrElse(Map())
 
-    sparkSession.read.format("mongo").options(mongoDBOptions).load()
 
+    val schema = schemaPath match {
+      case Some(path) => SchemaConverter.convert(FileUtils.readFileWithHadoop(path))
+      case None => sparkSession.loadFromMongoDB(ReadConfig(mongoDBOptions)).schema
+    }
+
+    buildDf(sparkSession, mongoDBOptions, schema)
+  }
+}
+
+object MongoDBInput {
+  private def buildDf(sparkSession: SparkSession, options: Map[String, String], schema: StructType): DataFrame = {
+    MongoSpark
+      .builder()
+      .sparkSession(sparkSession)
+      .readConfig(ReadConfig(options))
+      .build()
+      .toDF(stringifySchema(schema))
+  }
+
+  private def stringifySchema(schema : StructType): StructType = {
+    val fields = mutable.ArrayBuffer[StructField]()
+    schema.foreach(field => {
+      field.dataType match {
+        case struct : StructType => {
+          if (isSingleField(struct)) {
+            fields += StructField(field.name, StringType)
+          }
+          else {
+            fields += StructField(field.name, stringifySchema(struct))
+          }
+        }
+        case array : ArrayType => {
+          array.elementType match {
+            case struct: StructType => {
+              fields += StructField(field.name, ArrayType(stringifySchema(struct)))
+            }
+            case _ => fields += StructField(field.name, ArrayType(StringType))
+          }
+        }
+        case default => fields += StructField(field.name, StringType)
+      }
+    })
+    return StructType(fields)
+  }
+
+  private def isSingleField(struct : StructType): Boolean = {
+    struct.fields.size == 1
   }
 }
