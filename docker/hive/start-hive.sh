6d2e24adaf30e442a97d74a9b6055ef57b14db04
Amir Halatzi <amirhalatzi@users.noreply.github.com>
2022-12-11 10:29:31 +0200

    remove redundant assignment

--- docker/hive/start-hive.sh
+++ docker/hive/start-hive.sh
@@ -6,7 +6,6 @@ echo "Start Hive"
 METASTORE_PORT=${METASTORE_PORT:=9083}
 JMX_PORT=${JMX_PORT:=8008}
 JMX_PROMETHEUS_PORT=${JMX_PROMETHEUS_PORT:=8080}
-PROFILE=$1
 
 . ./start-metastore.sh "skip-init"
 . ./start-hiveserver.sh "skip-init"
