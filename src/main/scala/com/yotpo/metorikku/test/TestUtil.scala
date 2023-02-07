c893372ae081ba75cfed924a1232fedac117b215
Alvaro Santos Andres <Alvaro.Santos-1@syngenta.com>
2023-02-07 06:24:57 +0000

    feat: Tester can test complex structures

--- src/main/scala/com/yotpo/metorikku/test/TestUtil.scala
+++ src/main/scala/com/yotpo/metorikku/test/TestUtil.scala
@@ -4,6 +4,9 @@ import org.apache.log4j.LogManager
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions.{col, when}
 import scala.collection.mutable.ArrayBuffer
+import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.types.ArrayType
 
 object TestUtil {
 
@@ -23,8 +26,8 @@ object TestUtil {
 
   def getRowsFromDf(df: DataFrame): List[Map[String, Any]] = {
     df.rdd
-      .map { dfRow =>
-        dfRow.getValuesMap[Any](dfRow.schema.fieldNames)
+      .map { dfRow => 
+        rowToMap(dfRow)
       }
       .collect()
       .toList
@@ -96,4 +99,69 @@ object TestUtil {
     "\n" + new String(outCapture.toByteArray)
   }
 
+  private def getStructTypeFromStructType(field: String, schema: StructType): StructType =
+    schema.fields(schema.fieldIndex(field)).dataType.asInstanceOf[StructType]
+
+  private def getStructTypeFromArrayType(field: String, schema: StructType): StructType =
+    schema
+      .fields(schema.fieldIndex(field))
+      .dataType
+      .asInstanceOf[ArrayType]
+      .elementType
+      .asInstanceOf[StructType]
+
+  private def mapToRow(m: Map[String, Any], schema: StructType): Row = Row.fromSeq(
+    m.toList
+      .map {
+        case (key, struct: Map[String, Any] @unchecked) =>
+          schema.fieldIndex(key) -> mapToRow(struct, getStructTypeFromStructType(key, schema))
+        case (key, mapList)
+            if mapList.isInstanceOf[TraversableOnce[_]]
+              && mapList
+                .asInstanceOf[TraversableOnce[Any]]
+                .toSeq
+                .headOption
+                .exists(_.isInstanceOf[Map[_, _]]) =>
+          schema.fieldIndex(key) ->
+            mapList
+              .asInstanceOf[TraversableOnce[Any]]
+              .toSeq
+              .map(_.asInstanceOf[Map[String, Any]])
+              .map(mapToRow(_, getStructTypeFromArrayType(key, schema)))
+        case (key, None) =>
+          schema.fieldIndex(key) -> null
+        case (key, Some(other: Map[_, _])) =>
+          schema.fieldIndex(key) -> mapToRow(
+            other.asInstanceOf[Map[String, Any]],
+            getStructTypeFromStructType(key, schema)
+          )
+        case (key, Some(mapList))
+            if mapList.isInstanceOf[TraversableOnce[_]]
+              && mapList
+                .asInstanceOf[TraversableOnce[Any]]
+                .toSeq
+                .headOption
+                .exists(_.isInstanceOf[Map[_, _]]) =>
+          schema.fieldIndex(key) ->
+            mapList
+              .asInstanceOf[TraversableOnce[Any]]
+              .toSeq
+              .map(_.asInstanceOf[Map[String, Any]])
+              .map(mapToRow(_, getStructTypeFromArrayType(key, schema)))
+        case x @ (key, Some(other)) =>
+          schema.fieldIndex(key) -> other
+        case (key, other) =>
+          schema.fieldIndex(key) -> other
+      }
+      .sortBy(_._1)
+      .map(_._2)
+  )
+
+  private def rowToMap(row: Row): Map[String, Any] = row.schema.fieldNames
+    .zip(row.toSeq.map {
+      case row: Row                   => rowToMap(row)
+      case seqOfRow @ ((_: Row) :: _) => seqOfRow.map(_.asInstanceOf[Row]).map(rowToMap)
+      case any                        => any
+    })
+    .toMap
 }
